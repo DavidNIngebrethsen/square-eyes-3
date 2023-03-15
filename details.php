9c03ca49e7abe1b745775ebf9b1ad6a200178c34
DavidNIngebrethsen <111867398+DavidNIngebrethsen@users.noreply.github.com>
2023-03-15 14:03:49 +0100

    Wednesday upload for mount
    Uploading file so it can be mounted on a netlify site.

---
+++ details.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Square Eyes - Film X</title>
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="CMS-CA - Square Eyes - Details" content="Page for displaying detailed information about a movie">
+    <script src="https://kit.fontawesome.com/e56bb99359.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php
+    include "./header.php"
+    ?>
+
+    <div class="movie-entry"></div>
+
+    <?php
+    include "./footer.php"
+    ?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="mobile-bar">
+    <div class="mobile-bar-button"></div>
+    <div class="mobile-bar-button"></div>
+    <div class="mobile-bar-button"></div>
+    <div class="mobile-bar-button"></div>
+</footer>
