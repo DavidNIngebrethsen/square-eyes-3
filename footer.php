b9f0f87b8fdfdbe8cdb663153712358f6312a7d5
DavidNIngebrethsen <111867398+DavidNIngebrethsen@users.noreply.github.com>
2023-03-15 14:16:33 +0100

    Wednesday code replace
    php didn't work on netlify, so I've grabbed my old project code and pasted it in for now.

--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="mobile-bar">
-    <div class="mobile-bar-button"></div>
-    <div class="mobile-bar-button"></div>
-    <div class="mobile-bar-button"></div>
-    <div class="mobile-bar-button"></div>
-</footer>
