9e7831848a1600f87bf377e22e419e2867e988ca
DavidNIngebrethsen <111867398+DavidNIngebrethsen@users.noreply.github.com>
2023-03-20 14:12:23 +0100

    Monday upload 3
    Made first page work in giving the 3 first movies, entries link to details page, but details page has no info, but that should be solvable with search query.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 url = "https://nicko-skogen.no/square-eyes/wp-json/wc/store/products"
-category = "products"
 
 const container = document.querySelector(".frontpage-movies")
+const array = []
 
 async function getFilms() {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-}
-
-function printNewest3() {
-    const array = getFilms()
-    for (let i = array.length - 1; i > array.length - 3; i--) {
-        console.log(array[i])
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Data is now ready");
+    console.log(data)
+    for (let i = 0; i < 3; i++) {
+        console.log(data[i])
+        printFilm(data[i])
     }
+    array.push(data)
 }
+console.log("Before log");
+getFilms()
+console.log("after log");
 
-console.log(getFilms())
-
-/* printNewest3() */
+function printFilm(data) {
+    const link = document.createElement("a")
+    const box = document.createElement("div")
+    const img = document.createElement("img")
+    link.href = "details.html"
+    box.classList.add("film-entry", "button")
+    img.src = data.images[0].src
+    img.alt = data.images[0].alt
+    img.classList.add("film-poster")
+    box.append(img)
+    link.append(box)
+    container.append(link)
+}
